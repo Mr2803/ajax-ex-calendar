386c6973aaf71f4564cb1029516280661e46762c
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-20 17:22:41 +0100

    finish basic exercise

--- script/script.js
+++ script/script.js
@@ -17,23 +17,6 @@ Buon pome e rimanete in ascolto su questo canale per comunicazioni possibili nel
 
 $(document).ready(function () {
 
-/* var giorniMese = moment("2019-02", "YYYY-MM").daysInMonth(); */
-/* var genuary = moment("2018-01" , "YYYY-MM");
-console.log("questa variabile indica il mese di gennaio 2018" + genuary)
-
-var day1 = genuary.format("DD")
-console.log(day1) */
-   var meseAnno = moment("2018-01", "YYYY-MM");
-console.log("questo è il valore del mese che mi serve " + meseAnno);
-
-
-
-
-for (i = 1; i <= meseAnno.daysInMonth(); i++) {
-   console.log("giorno", i , "di", meseAnno.format("MMMM"))
-}
-
-//2015-10-31
 
 
 
@@ -42,7 +25,30 @@ for (i = 1; i <= meseAnno.daysInMonth(); i++) {
    $.ajax({
       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
       method: "GET",
-      success: function (data, stato) {
+      success: function (data) {
+         console.log(data.response)
+
+         var daysNumber = moment("2018-01-01", "YYYY-MM-DD").daysInMonth();
+         
+         for (i = 1; i <= daysNumber; i++) {
+            var dataCorrente = "2018-01-" + i;
+            var formatoData = moment(dataCorrente).format("YYYY-MM-DD")
+            var giornoCorrente = moment(dataCorrente).format("DD MMMM");
+            $(".calendar").append('<li data-date="' + formatoData +'">'+giornoCorrente+'</li>')
+
+           
+         }
+         for (var i = 0; i < data.response.length; i++) {
+            console.log("queste sono le date delle festività " + data.response[i].date)
+            var festivita = data.response[i].date  
+            $(".calendar [data-date = '" + festivita + "']").css("color","red")
+               /* $(".calendar").append('<li data-date="' + dataCorrente + '">' + giornoCorrente + '</li>').css("color","red") */
+
+            
+         }
+
+         
+
 
       },
       error: function (richiesta, stato, errori) {
